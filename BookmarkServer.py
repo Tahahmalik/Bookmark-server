f0cca19f447b46427a50d1a9d224914bafd4b22e
Taha Malik <taha.malik87@gmail.com>
2017-09-06 15:30:48 -0700

    Adding configuration files for Heroku Deployment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -1,6 +1,7 @@
 
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -101,6 +102,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT' , 8000))
-    server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
-    httpd.serve_forever()
+	server_address = ('', port)
+	httpd = http.server.HTTPServer(server_address, Shortener)
+	httpd.serve_forever()
